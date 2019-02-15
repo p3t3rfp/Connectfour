1399d75190593be7b51115bd756b06df0e1fd1c6
Peter Fitzpatrick <peterfitzpatrick@Peters-MBP.attlocal.net>
2019-02-14 19:23:58 -0500

    added pseudo code as well as board and player variables

---
+++ main.js
@@ -0,0 +1,37 @@
+//create the board
+
+var board = [
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','','']
+]
+
+
+//create modal to accept player names on start game button click and assign them red or blue
+
+var playerOne = {}
+var playerTwo = {}
+
+
+//attach click handler to start game button that loads the board and alerts playerOne it is their turn
+
+//attach click handler to game piece that will drop into board
+
+//add logic to alternate turns
+
+
+//attach click handler to clear board button that will reload board
+
+
+//logic to check vertical win
+
+
+//logic to check horizontal win
+
+
+//logic to check diagonal win
+
+
+//create counter to store player's number of wins
