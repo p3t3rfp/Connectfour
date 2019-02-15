2e7a62008e280c3c9bf97b911a35a3f371c22dcd
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-02-15 11:15:07 -0500

    added alternating turn functionality visually

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //create the board
 
 var board = [
-    ['','','','','','',''],
-    ['','','','','','',''],
-    ['','','','','','',''],
-    ['','','','','','',''],
-    ['','','','','','','']
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '']
 ]
 
 
@@ -30,19 +30,36 @@ function loadBoard() {
 
 //attach click handler to start game button that loads the board and alerts playerOne it is their turn
 
-$('.getStarted').on('click', function() {
+$('.getStarted').on('click', function () {
     loadBoard()
-    alert(`${playerOne.name} it is your turn`)
+    // alert(`${playerOne.name} it is your turn`)
     $('.getStarted').hide()
 })
 
-//attach click handler to game piece that will drop into board
+//attach click handler to game piece that will follow mouse and drop into board column
+
 
 //add logic to alternate turns
+
+let counter = 2
+// let player = [playerOne, playerTwo]
+
+$('.blank').on('click', function (player) {
+    console.log('clicked blank')
+        if (counter % 2 === 0) {
+            $(this).toggleClass('blank').addClass('blue')
+        } else {
+            $(this).toggleClass('blank').addClass('red')
+        }
+        counter += 1
+})
 
 
 //attach click handler to clear board button that will reload board
 
+// $('.clearBoard').on('click', function() {
+//     $('.board').
+// })
 
 //logic to check vertical win
 
