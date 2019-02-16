00d2ed6d5295cd726dbe729d0a9b09cc2e75545e
Peter Fitzpatrick <peterfitzpatrick@Peters-MBP.attlocal.net>
2019-02-16 10:47:49 -0500

    vertical win functions

--- main.js
+++ main.js
@@ -40,19 +40,17 @@ $('.getStarted').on('click', function () {
     $('.getStarted').hide()
 })
 
-//attach click handler to game piece that will follow mouse and drop into board column
-
-
-//add logic to alternate turns
+//attach click handler to game piece that will place into board column
 
 let counter = 2
-// let player = [playerOne, playerTwo]
-let loc
-//function to not allow a piece to be overridden or clicked again
+
 $('.blank').on('click', function () {
+
+    //function to not allow a piece to be overridden or clicked again
     if ($(this).hasClass('blue') || $(this).hasClass('red') || $(this).next().hasClass('blank')) {
         return
     }
+    //logic to alternate turns
     if (counter % 2 === 0) {
         $(this).toggleClass('blank').addClass('blue')
         $('.piece').css('background-color', 'red')
@@ -60,15 +58,23 @@ $('.blank').on('click', function () {
         $(this).toggleClass('blank').addClass('red')
         $('.piece').css('background-color', 'blue')
     }
+
+    //place each selected piece into the gameBoard array by color
     counter += 1
     rowLocation = this
     columnLocation = this.parentNode
     row = parseInt(rowLocation.classList[0])
     column = parseInt(columnLocation.classList[1])
     color = rowLocation.classList[1]
-    console.log(`${row} is row and ${column} is column`)
+    // console.log(`${row} is row and ${column} is column`)
     gameBoard[row][column] = color
-    horizontal()
+
+    if (color === 'blue') {
+        checkForWin(gameBoard, 'blue')
+    } else {
+        checkForWin(gameBoard, 'red')
+    }
+    return false
 })
 
 
@@ -84,13 +90,46 @@ $('.clearBoard').on('click', function () {
 
 //logic to check vertical win
 
+function verticalWin(array, color) {
+    for (let i = 0; i < 5; i++) {
+        if (array[0][i] === color
+            && array[1][i] === color
+            && array[2][i] === color
+            && array[3][i] === color
+        ) alert(`${color} wins!`)
+
+        if (array[1][i] === color
+            && array[2][i] === color
+            && array[3][i] === color
+            && array[4][i] === color
+        ) alert(`${color} wins!`)
+        
+        if (array[2][i] === color
+            && array[3][i] === color
+            && array[4][i] === color
+            && array[5][i] === color
+        ) alert(`${color} wins!`)
+    }
+    return false
+}
 
 //logic to check horizontal win
 
-function check4horizontal(array) {
-    return array.some(function (a, j, aa) {
-        return j > 1 && a === aa[j - 3] && a === aa[j - 2] && a === aa[j - 1];
-    });
+function horizontalWin(array, color) {
+    for (let i = 0; i < 6; i++) {
+        if (array[i][0] === color
+            && array[i][1] === color
+            && array[i][2] === color
+            && array[i][3] === color
+        ) alert(`${color} wins!`);
+
+        if (array[i][1] === color
+            && array[i][2] === color
+            && array[i][3] === color
+            && array[i][4] === color
+        ) alert(`${color} wins!`);
+    }
+    return false;
 }
 
 // function horizontal() {
@@ -99,14 +138,23 @@ function check4horizontal(array) {
 //     for (let i = 0; i < gameBoard.length; i ++) {
 //         for (let j = 0; j < gameBoard[i].length; j ++) {
 //             if (gameBoard[i][j] = blue) {
-//                 check4horizontal(gameBoard)
 //             } 
 //         }
 //     }
 // }
 
 
-//logic to check diagonal win
+//logic to check diagonal wins
+
+//function to check for game winner by color includes all 4 variations - vertical/horizontal
+//forward diagonal and backward diagonal
+
+function checkForWin(array, color) {
+    return verticalWin(array, color)
+        || horizontalWin(array, color)
+        // || checkDiagonal1(array, color)
+        // || checkDiagonal2(array, color)
+}
 
 
 //create counter to store player's number of wins
